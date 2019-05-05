1f833727d6b0f45c76df88828e3dcf1c80699a08
Fed Kwok <fedomain@gmail.com>
2019-05-05 23:15:07 +1000

    adding operator file

--- operator.cpp
+++ operator.cpp
@@ -2,17 +2,78 @@
 
 using namespace std;
 
-struct Datasource
+struct Test
 {
-        struct Iterator
-        {
-                virtual bool operator==(const Iterator&) = 0;
-        };
+        private:
+                int count;
+
+        public:
+                Test(): count(5) {}
+
+                Test operator ++() {
+                        Test temp;
+                        temp.count = ++count;
+
+                        return temp;
+                }
+
+                Test operator ++(int) {
+                        Test temp;
+                        temp.count = count++;
+
+                        return temp;
+                }
+
+                void fun() const { 
+                        cout << "fun() const called " << endl; 
+                }
+
+                void fun() { 
+                        cout << "fun() called " << endl; 
+                }
+
+                void Display() { 
+                        cout << "Count: " << count << endl;
+                }
 };
-
-
 
 int main(void) 
 {
+        Test t1;
+        const Test t2;
+
+        t1.fun();
+        t2.fun();
+
+        const int i = 1;
+        const int j = 2;
+        int k = 3;
+
+        int * const c = &k;
+
+        cout << k << endl;
+        cout << c << endl;
+        cout << *c << endl;
+
+        k = 4;
+
+        cout << k << endl;
+        cout << c << endl;
+        cout << *c << endl;
+        
+/*
+        t.Display();
+        t1.Display();
+
+        t1 = ++t;
+        t.Display();
+        t1.Display();
+
+        t1 = t++;
+        t.Display();
+        t1.Display();
+
+        cout << 3 * 2 << endl;
+*/
         return 0;
 }
